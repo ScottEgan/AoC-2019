caf4acc85f428b20a94dfe7d8d62e750bfa3da01
ScottEgan <ScottEgan@users.noreply.github.com>
2019-12-11 17:28:37 -0800

    day 11 fixed memory issues

--- Day 11/Space Police Part 1.py
+++ Day 11/Space Police Part 1.py
@@ -31,12 +31,6 @@ class IntcodeComputer(object):
         self.phaseSetting = 0
         self.someInput = 0
 
-    def get_memory(self):
-        """
-        """
-        return self.memory.copy()
-    
-
     def parseInstruction(self, instruction):
         """
         Returns:
@@ -58,89 +52,87 @@ class IntcodeComputer(object):
         else:
             return int(str(instruction)[-2:]), int(str(instruction)[-3:-2]), int(str(instruction)[-4:-3]), int(str(instruction)[-5:-4])
 
-    def getValue(self, mode, parameter, memory):
+    def getValue(self, mode, parameter):
         """
         """
-        #memory = self.get_memory()
 
         if mode == 0:
             try:
-                return memory[memory[parameter]]
+                return self.memory[self.memory[parameter]]
             except IndexError:
-                memory.extend([0] * (memory[parameter] - (len(memory) - 1)))
-                return memory[memory[parameter]]
+                self.memory.extend([0] * (self.memory[parameter] - (len(self.memory) - 1)))
+                return self.memory[self.memory[parameter]]
 
         elif mode == 1:
             try:
-                return memory[parameter]
+                return self.memory[parameter]
             except IndexError:
-                memory.extend([0] * (parameter - (len(memory) - 1)))
-                return memory[parameter]
+                self.memory.extend([0] * (parameter - (len(self.memory) - 1)))
+                return self.memory[parameter]
 
         else:
             try:
-                return memory[self.relativeBase + memory[parameter]]
+                return self.memory[self.relativeBase + self.memory[parameter]]
             except IndexError:
-                memory.extend([0] * ((self.relativeBase + memory[parameter]) - (len(memory) - 1)))
-                return memory[self.relativeBase + memory[parameter]]
+                self.memory.extend([0] * ((self.relativeBase + self.memory[parameter]) - (len(self.memory) - 1)))
+                return self.memory[self.relativeBase + self.memory[parameter]]
 
-    def setValue(self, mode, parameter, value, memory):
+    def setValue(self, mode, parameter, value):
         """
         """
         if mode == 0:
             try:
-                memory[memory[parameter]] = value
+                self.memory[self.memory[parameter]] = value
             except IndexError:
-                memory.extend([0] * (memory[parameter] - (len(memory) - 1)))
-                memory[memory[parameter]] = value
-            #print(f"setting memory at {memory[parameter]} to {value}")
+                self.memory.extend([0] * (self.memory[parameter] - (len(self.memory) - 1)))
+                self.memory[self.memory[parameter]] = value
+            #print(f"setting self.memory at {self.memory[parameter]} to {value}")
 
         elif mode == 1:
             try:
-                memory[parameter] = value
+                self.memory[parameter] = value
             except IndexError:
-                memory.extend([0] * (parameter - (len(memory) - 1)))
-                memory[parameter] = value
-            #print(f"setting memory at {parameter} to {value}")
+                self.memory.extend([0] * (parameter - (len(self.memory) - 1)))
+                self.memory[parameter] = value
+            #print(f"setting self.memory at {parameter} to {value}")
 
         else:
             try:
-                memory[self.relativeBase + memory[parameter]] = value
+                self.memory[self.relativeBase + self.memory[parameter]] = value
             except IndexError:
-                memory.extend([0] * ((self.relativeBase + memory[parameter]) - (len(memory) - 1)))
-                memory[self.relativeBase + memory[parameter]] = value
-            #print(f"setting memory at {self.relativeBase + memory[parameter]} to {value}")
+                self.memory.extend([0] * ((self.relativeBase + self.memory[parameter]) - (len(self.memory) - 1)))
+                self.memory[self.relativeBase + self.memory[parameter]] = value
+            #print(f"setting self.memory at {self.relativeBase + self.memory[parameter]} to {value}")
 
     def Compute(self, index, input):
         """Mostly copied from Part 1
         """
-        memory = self.get_memory()
         self.phaseSetting = input
         self.someInput = input
         inputNum = 1
         currentIndex = index
         output = []
-        while currentIndex != 'end' and currentIndex < len(memory):
+        while currentIndex != 'end' and currentIndex < len(self.memory):
 
-            if memory[currentIndex] == 3 or memory[currentIndex] == 4:
-                #print(f"instruction : {memory[currentIndex]}")
-                opCode, p1, p2, p3 = memory[currentIndex], 0, 0, 0
+            if self.memory[currentIndex] == 3 or self.memory[currentIndex] == 4:
+                #print(f"instruction : {self.memory[currentIndex]}")
+                opCode, p1, p2, p3 = self.memory[currentIndex], 0, 0, 0
             else:
-                opCode, p1, p2, p3 = self.parseInstruction(memory[currentIndex])
+                opCode, p1, p2, p3 = self.parseInstruction(self.memory[currentIndex])
             #print(f"opcode:{opCode}, p1:{p1}, p2:{p2}, p3:{p3}")
 
             if opCode == 1:
                 #print("1 Add")
-                toSet = self.getValue(p1, currentIndex + 1, memory) + self.getValue(p2, currentIndex + 2, memory)
-                self.setValue(p3, currentIndex + 3, toSet, memory)
+                toSet = self.getValue(p1, currentIndex + 1) + self.getValue(p2, currentIndex + 2)
+                self.setValue(p3, currentIndex + 3, toSet)
                 currentIndex += 4
                 #print(f"current index: {currentIndex}")
                 
 
             elif opCode == 2:
                 #print("2 Multiply")
-                toSet = self.getValue(p1, currentIndex + 1, memory) * self.getValue(p2, currentIndex + 2, memory)
-                self.setValue(p3, currentIndex + 3, toSet, memory)
+                toSet = self.getValue(p1, currentIndex + 1) * self.getValue(p2, currentIndex + 2)
+                self.setValue(p3, currentIndex + 3, toSet)
                 currentIndex += 4
                 #print(f"current index: {currentIndex}")
                 
@@ -149,21 +141,21 @@ class IntcodeComputer(object):
                 if inputNum == 1:
                     #set phase
                     #print("3 Input")
-                    self.setValue(p1, currentIndex + 1, self.phaseSetting, memory)
+                    self.setValue(p1, currentIndex + 1, self.phaseSetting)
                     currentIndex += 2
                     inputNum += 1
                     #print(f"current index: {currentIndex}")
                     
                 else:
                     #print("3 Input")
-                    self.setValue(p1, currentIndex + 1, self.someInput, memory)
+                    self.setValue(p1, currentIndex + 1, self.someInput)
                     currentIndex += 2
                     #print(f"current index: {currentIndex}")
                     
 
             elif opCode == 4:
                 #print("4 Output")
-                output.append(self.getValue(p1, currentIndex + 1, memory))
+                output.append(self.getValue(p1, currentIndex + 1))
                 currentIndex += 2
                 #print(f"current index: {currentIndex}")
                 if len(output) == 2:
@@ -172,8 +164,8 @@ class IntcodeComputer(object):
 
             elif opCode == 5:
                 #print("5 jump-if-true")
-                if self.getValue(p1, currentIndex + 1, memory) != 0:
-                    currentIndex = self.getValue(p2, currentIndex + 2, memory)
+                if self.getValue(p1, currentIndex + 1) != 0:
+                    currentIndex = self.getValue(p2, currentIndex + 2)
                     #print(f"jumping to index {currentIndex}")
                 else:
                     currentIndex += 3
@@ -181,8 +173,8 @@ class IntcodeComputer(object):
 
             elif opCode == 6:
                 #print("6 jump-if-false")
-                if self.getValue(p1, currentIndex + 1, memory) == 0:
-                    currentIndex = self.getValue(p2, currentIndex + 2, memory)
+                if self.getValue(p1, currentIndex + 1) == 0:
+                    currentIndex = self.getValue(p2, currentIndex + 2)
                     #print(f"jumping to index {currentIndex}")
                 else:
                     currentIndex += 3
@@ -190,37 +182,38 @@ class IntcodeComputer(object):
 
             elif opCode == 7:
                 #print("7 less than")
-                if self.getValue(p1, currentIndex + 1, memory) < self.getValue(p2, currentIndex + 2, memory):
-                    self.setValue(p3, currentIndex + 3, 1, memory)
+                if self.getValue(p1, currentIndex + 1) < self.getValue(p2, currentIndex + 2):
+                    self.setValue(p3, currentIndex + 3, 1)
                     currentIndex += 4
                     #print(f"current index: {currentIndex}")
 
                 else:
-                    self.setValue(p3, currentIndex + 3, 0, memory)
+                    self.setValue(p3, currentIndex + 3, 0)
                     currentIndex += 4
                     #print(f"current index: {currentIndex}")
 
             elif opCode == 8:
                 #print("8 equals")
-                if self.getValue(p1, currentIndex + 1, memory) == self.getValue(p2, currentIndex + 2, memory):
-                    self.setValue(p3, currentIndex + 3, 1, memory)
+                if self.getValue(p1, currentIndex + 1) == self.getValue(p2, currentIndex + 2):
+                    self.setValue(p3, currentIndex + 3, 1)
                     currentIndex += 4
                     #print(f"current index: {currentIndex}")
 
                 else:
-                    self.setValue(p3, currentIndex + 3, 0, memory)
+                    self.setValue(p3, currentIndex + 3, 0)
                     currentIndex += 4
                     #print(f"current index: {currentIndex}")
 
             elif opCode == 9:
                 #print("9 relative base")
-                self.relativeBase += self.getValue(p1, currentIndex + 1, memory)
+                self.relativeBase += self.getValue(p1, currentIndex + 1)
                 currentIndex += 2
                 #print(f"current index: {currentIndex}")
                 
             else:
                 #print("99 End")
                 currentIndex = 'end'
+                return output, currentIndex
                 
         
         return output, currentIndex
@@ -243,25 +236,36 @@ y = 0
 ylist = []
 fig = plt.figure()
 
-while currentIndex != 'end' and step < 50:
+plot = True
+
+while currentIndex != 'end' and step < 150:
+
     xlist.append(x)
     ylist.append(y)
-    coord[(x, y)] = 0
-    print(f"robot at {x}, {y} facing {direction} tile is {coord[(x, y)]}")
+    
+    if (x, y) not in coord.keys():
+        coord[(x, y)] = 0
+
+    print(f"robot at ({x}, {y}) facing {direction} tile is {coord[(x, y)]}")
+    print(f"starting robot with index {currentIndex} and input {coord[(x, y)]}")
+
     output, currentIndex = robot.Compute(currentIndex, coord[(x, y)])
 
     print(output, currentIndex)
 
     if output[0] == 0:
         #paint panel black
+        print(f"paint panel black at {x}, {y}")
         coord[(x, y)] = 0
     else:
         #paint panel white
+        print(f"paint panel white at {x}, {y}")
         coord[(x, y)] = 1
     
     if output[1] == 0:
         #robot turns left 90 deg
         #and moves forward 1 space
+        print("robot turns to the left")
         if direction == 'N':
             direction = 'W'
             x -= 1
@@ -277,6 +281,7 @@ while currentIndex != 'end' and step < 50:
     else:
         #robot turns right 90 deg
         #and moves forward 1 space
+        print("robot turns to the right")
         if direction == 'N':
             direction = 'E'
             x += 1
@@ -290,10 +295,18 @@ while currentIndex != 'end' and step < 50:
             direction = 'N'
             y += 1
 
-    plt.cla()
-    plt.axis([-10, 5, -5, 30])
-    plt.plot(xlist, ylist, 'b-')
-    plt.pause(0.2)
-    step += 1
+    if plot:
+        plt.cla()
+        plt.axis([-20, 20, -20, 20])
+        plt.plot(xlist, ylist, 'b-')
+        plt.pause(0.2)
 
-plt.show()
+    step += 1
+    print(f"step number: {step}")
+    print(f"robot at {x}, {y} facing {direction}")
+    print("")
+
+print("END")
+print(len(coord.keys())) 
+if plot:
+    plt.show()
