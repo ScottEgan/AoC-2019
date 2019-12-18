2afa954b3e5a7890e7d9df2a8e78cd601c180eed
ScottEgan <ScottEgan@users.noreply.github.com>
2019-12-18 14:10:44 -0800

    slight clean up

--- Day 14/Space Stoichiometry part 1.py
+++ Day 14/Space Stoichiometry part 1.py
@@ -2,6 +2,9 @@
 tried depth first search and it gave the wrong answers.
 will have to do breadth first search
 
+new strategy V5:
+1: keep BETTER track of extra production
+
 1 ORE => 2 A
 1 A => 1 B
 1 B => 1 C
@@ -18,18 +21,19 @@ for elm in lines:
     for i in range(2):
         elm[i] = elm[i].strip()
         elm[i] = elm[i].split(",")
-   
 #print(lines)
 
+# split things up into a nice dictionary where the keys are the products
+# the first value is the ammount prouced and the second is a list of reactants
 d = {}
 for elm in lines:
     d[elm[1][0].split()[1]] = (int(elm[1][0].split()[0]), [])
     for i in range(len(elm[0])):
         elm[0][i] = elm[0][i].strip()
         d[elm[1][0].split()[1]][1].append((int(elm[0][i].split()[0]), elm[0][i].split()[1]))
+#print(f"input: {d}")
 
-print(f"input: {d}")
-
+# find a list of all elements
 allElements = {}
 for elm in d.keys():
     if elm not in allElements:
@@ -37,33 +41,25 @@ for elm in d.keys():
     for i in range(len(d[elm][1])):
         if d[elm][1][i][1] not in allElements:
             allElements[d[elm][1][i][1]] = 0
-
-print(f"all elements: {allElements}")
+#print(f"all elements: {allElements}")
  
-
-
-# new strategy V4:
-# 1: keep track of extra production
-# 4: profit
-
+# find the elements that are produced from ore
 basics = {}
 for elm in d.keys():
     if d[elm][1][0][1] == "ORE":
         basics[elm] = 0
+#print(f"basics: {basics}")
 
-print(f"basics: {basics}")
-
-# do a breadth first search to find requirements for second level elements
+# do a breadth first search to find the ammount of ore needed
 def BFS(inputElement, allElements):
     queue = []
-    queuesim = []
-
+    queuesim = []       #this is just a helper list
     for i in range(len(d[inputElement][1])):
         queue.append([d[inputElement][1][i][1], d[inputElement][1][i][0]])
         queuesim.append(d[inputElement][1][i][1])
+    #print(f"queue: {queue}")
+    #print(f"queue simple: {queuesim}")
 
-    print(f"queue: {queue}")
-    print(f"queue simple: {queuesim}")
     ore = 0
     while queue:
         tmp = queue.pop(0)
@@ -73,6 +69,7 @@ def BFS(inputElement, allElements):
         qtyElmProd = d[elmName][0]
         reactList = d[elmName][1]
 
+        #this block helps keep track of waste elements
         if allElements[elmName] > 0:
             if allElements[elmName] > qtyElmReq:
                 allElements[elmName] -= qtyElmReq
@@ -87,21 +84,28 @@ def BFS(inputElement, allElements):
                 useOre = False
         else:
             useOre = True
-
+        # enter here if the item doesn't brake down into ore and we
+        # didn't fulfill the requirements from the waste 
         if elmName not in basics.keys() and useOre:
+            # check to see if we produce waste - if so keep track of it
             if (math.ceil(qtyElmReq / qtyElmProd) * qtyElmProd) > qtyElmReq:
                 allElements[elmName] += ((math.ceil(qtyElmReq / qtyElmProd) * qtyElmProd) - qtyElmReq)
             
             for i in range(len(reactList)):
                 reactName = reactList[i][1]
                 qtyReact = reactList[i][0]
+                #sum items in the queue for simplification
                 if reactName in queuesim:
                     queue[queuesim.index(reactName)][1] += (math.ceil(qtyElmReq / qtyElmProd) * qtyReact)
                 else:
                     queue.append([reactName, math.ceil(qtyElmReq / qtyElmProd) * qtyReact])
                     queuesim.append(reactName)
+
+        # enter here if the item does brake down into ore and we
+        # didn't fulfill the requirements from the waste 
         elif useOre:
             ore += math.ceil(qtyElmReq / qtyElmProd) * reactList[0][0]
+            # check to see if we produce waste - if so keep track of it
             if (math.ceil(qtyElmReq / qtyElmProd) * qtyElmProd) > qtyElmReq:
                 allElements[elmName] += ((math.ceil(qtyElmReq / qtyElmProd) * qtyElmProd) - qtyElmReq)
 
