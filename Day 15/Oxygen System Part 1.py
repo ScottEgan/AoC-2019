70c54e8c93b0ed384f3de45733944b88ed45c590
ScottEgan <ScottEgan@users.noreply.github.com>
2019-12-18 15:24:33 -0800

    initial commit

---
+++ Day 15/Oxygen System Part 1.py
@@ -0,0 +1,292 @@
+"""
+Intcode maze robot
+"""
+
+import matplotlib.pyplot as plt
+import time
+
+def load_file(filename):
+    """Loads a file to a list
+
+    Parameters:
+    filename (string): the name of the file containing input on a
+    single line
+
+    Returns: 
+    a (list) of ints
+    """
+    inputList = []
+    print(f"Loading list from file... {filename}") 
+    with open(filename) as file:
+        inputList = [int(x) for x in file.read().split(",")]
+
+    return inputList
+
+class IntcodeComputer(object):
+    """
+    """
+    def __init__(self, memory):
+        """
+        """
+        self.memory = memory
+        self.relativeBase = 0
+        self.phaseSetting = 0
+        self.someInput = 0
+
+    def parseInstruction(self, instruction):
+        """
+        Returns:
+        opCode, p1, p2, p3
+        """
+        #print(f"instruction : {instruction}")
+
+        if len(str(instruction)) == 1:
+            return instruction, 0, 0, 0
+
+        elif instruction == 99:
+            return instruction, 0, 0, 0
+        
+        elif len(str(instruction)) == 3:
+            return int(str(instruction)[-2:]), int(str(instruction)[-3:-2]), 0, 0
+
+        elif len(str(instruction)) == 4:
+            return int(str(instruction)[-2:]), int(str(instruction)[-3:-2]), int(str(instruction)[-4:-3]), 0
+        else:
+            return int(str(instruction)[-2:]), int(str(instruction)[-3:-2]), int(str(instruction)[-4:-3]), int(str(instruction)[-5:-4])
+
+    def getValue(self, mode, parameter):
+        """
+        """
+
+        if mode == 0:
+            try:
+                return self.memory[self.memory[parameter]]
+            except IndexError:
+                self.memory.extend([0] * (self.memory[parameter] - (len(self.memory) - 1)))
+                return self.memory[self.memory[parameter]]
+
+        elif mode == 1:
+            try:
+                return self.memory[parameter]
+            except IndexError:
+                self.memory.extend([0] * (parameter - (len(self.memory) - 1)))
+                return self.memory[parameter]
+
+        else:
+            try:
+                return self.memory[self.relativeBase + self.memory[parameter]]
+            except IndexError:
+                self.memory.extend([0] * ((self.relativeBase + self.memory[parameter]) - (len(self.memory) - 1)))
+                return self.memory[self.relativeBase + self.memory[parameter]]
+
+    def setValue(self, mode, parameter, value):
+        """
+        """
+        if mode == 0:
+            try:
+                self.memory[self.memory[parameter]] = value
+            except IndexError:
+                self.memory.extend([0] * (self.memory[parameter] - (len(self.memory) - 1)))
+                self.memory[self.memory[parameter]] = value
+            #print(f"setting self.memory at {self.memory[parameter]} to {value}")
+
+        elif mode == 1:
+            try:
+                self.memory[parameter] = value
+            except IndexError:
+                self.memory.extend([0] * (parameter - (len(self.memory) - 1)))
+                self.memory[parameter] = value
+            #print(f"setting self.memory at {parameter} to {value}")
+
+        else:
+            try:
+                self.memory[self.relativeBase + self.memory[parameter]] = value
+            except IndexError:
+                self.memory.extend([0] * ((self.relativeBase + self.memory[parameter]) - (len(self.memory) - 1)))
+                self.memory[self.relativeBase + self.memory[parameter]] = value
+            #print(f"setting self.memory at {self.relativeBase + self.memory[parameter]} to {value}")
+
+    def Compute(self, input):
+        """Mostly copied from Part 1
+        """
+        self.phaseSetting = input
+        self.someInput = input
+        currentIndex = 0
+        output = []
+        while currentIndex != 'end' and currentIndex < len(self.memory):
+
+            #print(f"index is: {currentIndex}")
+            if self.memory[currentIndex] == 3 or self.memory[currentIndex] == 4:
+                #print(f"instruction : {self.memory[currentIndex]}")
+                opCode, p1, p2, p3 = self.memory[currentIndex], 0, 0, 0
+            else:
+                opCode, p1, p2, p3 = self.parseInstruction(self.memory[currentIndex])
+            #print(f"opcode:{opCode}, p1:{p1}, p2:{p2}, p3:{p3}")
+
+            if opCode == 1:
+                #print("1 Add")
+                toSet = self.getValue(p1, currentIndex + 1) + self.getValue(p2, currentIndex + 2)
+                self.setValue(p3, currentIndex + 3, toSet)
+                currentIndex += 4
+                #print(f"current index: {currentIndex}")
+                
+            elif opCode == 2:
+                #print("2 Multiply")
+                toSet = self.getValue(p1, currentIndex + 1) * self.getValue(p2, currentIndex + 2)
+                self.setValue(p3, currentIndex + 3, toSet)
+                currentIndex += 4
+                #print(f"current index: {currentIndex}")
+                
+            elif opCode == 3:
+                #print("3 Input")
+                self.setValue(p1, currentIndex + 1, self.someInput)
+                currentIndex += 2
+                #print(f"current index: {currentIndex}")       
+
+            elif opCode == 4:
+                #print("4 Output")
+                return self.getValue(p1, currentIndex + 1)
+                #currentIndex += 2
+                #print(f"current index: {currentIndex}")
+                
+            elif opCode == 5:
+                #print("5 jump-if-true")
+                if self.getValue(p1, currentIndex + 1) != 0:
+                    currentIndex = self.getValue(p2, currentIndex + 2)
+                    #print(f"jumping to index {currentIndex}")
+                else:
+                    currentIndex += 3
+                    #print(f"current index: {currentIndex}")
+
+            elif opCode == 6:
+                #print("6 jump-if-false")
+                if self.getValue(p1, currentIndex + 1) == 0:
+                    currentIndex = self.getValue(p2, currentIndex + 2)
+                    #print(f"jumping to index {currentIndex}")
+                else:
+                    currentIndex += 3
+                    #print(f"current index: {currentIndex}")
+
+            elif opCode == 7:
+                #print("7 less than")
+                if self.getValue(p1, currentIndex + 1) < self.getValue(p2, currentIndex + 2):
+                    self.setValue(p3, currentIndex + 3, 1)
+                    currentIndex += 4
+                    #print(f"current index: {currentIndex}")
+
+                else:
+                    self.setValue(p3, currentIndex + 3, 0)
+                    currentIndex += 4
+                    #print(f"current index: {currentIndex}")
+
+            elif opCode == 8:
+                #print("8 equals")
+                if self.getValue(p1, currentIndex + 1) == self.getValue(p2, currentIndex + 2):
+                    self.setValue(p3, currentIndex + 3, 1)
+                    currentIndex += 4
+                    #print(f"current index: {currentIndex}")
+
+                else:
+                    self.setValue(p3, currentIndex + 3, 0)
+                    currentIndex += 4
+                    #print(f"current index: {currentIndex}")
+
+            elif opCode == 9:
+                #print("9 relative base")
+                self.relativeBase += self.getValue(p1, currentIndex + 1)
+                currentIndex += 2
+                #print(f"current index: {currentIndex}")
+                
+            else:
+                #print("99 End")
+                currentIndex = 'end'
+                return currentIndex
+                
+        
+        return output
+
+#load memory
+memory = load_file("Day 15/input.txt")
+
+robot = IntcodeComputer(memory.copy())
+
+# dictionary to hold coordinates (x, y) and what is there:
+# 0 => not explored
+# 1 => path
+# 2 => o2 system
+# 3 => wall
+coord = {(0, 0): 1}
+
+# initial conditions
+step = 0
+direction = 1
+x = 0
+y = 0
+end = False
+while step < 10000 or not end:
+
+    output = robot.Compute(direction)
+
+    #wall in direction
+    if output == 0:
+        # trying to go north
+        if direction == 1:
+            direction = 4  # instead go east
+            coord[(x, y + 1)] = 3
+            
+        # trying to go east
+        elif direction == 4:
+            direction = 2  # instead go south
+            coord[(x + 1, y)] = 3
+            
+        # trying to go south
+        elif direction == 2:
+            direction = 3  # instead go west
+            coord[(x, y - 1)] = 3
+            
+        #trying to go west
+        elif direction == 3:
+            direction = 1  # instead go noth
+            coord[(x - 1, y)] = 3
+            
+    # moved one step in direction passed
+    elif output == 1:
+        # going north
+        if direction == 1:
+            y += 1
+            
+        # going east
+        elif direction == 4:
+            x += 1
+            
+        # going south
+        elif direction == 2:
+            y -= 1
+
+        # going west
+        elif direction == 3:
+            x -= 1
+        
+        coord[(x, y)] = 1
+
+    #found o2 system
+    elif output == 2:
+        # going north
+        if direction == 1:
+            y += 1
+        # going east
+        elif direction == 4:
+            x += 1
+        # going south
+        elif direction == 2:
+            y -= 1
+        # going west
+        elif direction == 3:
+            x -= 1
+        
+        coord[(x, y)] = 2
+        end = True
+
+    step += 1
+
+
