d293195d15b46aa2957cb838e24bcbc400fee496
Scott Laptop <thescottegan@gmail.com>
2019-12-24 10:20:22 -0800

    small changes

--- Day 18/Many-Worlds Interpretation Part 1.py
+++ Day 18/Many-Worlds Interpretation Part 1.py
@@ -29,39 +29,48 @@ for index, value in np.ndenumerate(maze):
         elif value == '1':
             path.add(index)
     else:
-        maze[index] = int(ord(value))
+        maze[index] = int(ord(value))   #convert letters to ASCII numbers for plotting
         if value.isupper():
             doors[index] = value
+            #path.add(index)
         else:
             keys[index] = value
+            #path.add(index)
 
 maze = maze.astype(np.int, copy=False)
 
-print(doors)
-print(keys)
-print(robot)
-print(path)
+print(f"doors: {doors}")
+print(f"keys: {keys}")
+print(f"robot: {robot}")
+print(f"path {path}")
 #print(maze)
 
 possibleMovement = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 moveQueue = [robot]
 keyList = []
+stateList = {}
 step = 0
 # not sure what to do here in order to solve this. may need to look into path finding algorithms
 while step < 10:
     currentCoord = moveQueue.pop(0)
+    step += 1
+
     for elm in possibleMovement:
         updatedCoord = tuple(e + v for e, v in zip(elm, currentCoord))
         if updatedCoord in path:
             moveQueue.append(updatedCoord)
-        elif updatedCoord in keys:
+            stateList[updatedCoord] = (step, keyList)
+        elif updatedCoord in (path & set(keys)):
             moveQueue.append(updatedCoord)
             keyList.append(keys[updatedCoord])
+            # if you find a key remove the door by adding it to the list of accepted paths
+            path.add(doors[[k for k, v in doors.items() if v == keys[updatedCoord].upper()][0]])
+            stateList[updatedCoord] = (step, keyList)
     
-    step += 1
+    
 
 
-plot = True
+plot = False
 if plot:
     # color map:
     # 1-2 white => path
