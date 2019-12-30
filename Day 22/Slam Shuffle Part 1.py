a0cbb83ff454d55944c48be8113c9869d0e4c736
Scott Laptop <thescottegan@gmail.com>
2019-12-30 12:57:33 -0800

    solutions day 22

--- Day 22/Slam Shuffle Part 1.py
+++ Day 22/Slam Shuffle Part 1.py
@@ -1,10 +1,15 @@
+"""
+deal into new stack = 1
+cut = 2
+deal with increment = 3
+"""
+
 import numpy as np
 
 
-with open("Day 22/inputTest.txt") as file:
+with open("Day 22/input.txt") as file:
     input = [line.strip() for line in file]
-print(input)
-deck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(input)
 
 def dealNewStack(deck):
     d = deck.copy()
@@ -27,5 +32,26 @@ def dealWithIncr(deck, num):
             
         s[index] = d.pop(0)
     return list(s)
+
+# deck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+deck = list(range(10007))   
+#print(deck)
+
+#taskList = []
+for elm in input:
     
-print(deck)
+    if elm.split()[0] == 'cut':
+        #taskList.append((2, int(elm.split()[1])))
+        newDeck = cut(deck, int(elm.split()[1]))
+    elif elm.split()[0] == 'deal':
+        if elm.split()[1] == 'with':
+            #taskList.append((3, int(elm.split()[3])))
+            newDeck = dealWithIncr(deck, int(elm.split()[3]))
+        elif elm.split()[1] == 'into':
+            #taskList.append((1, 0))
+            newDeck = dealNewStack(deck)
+    deck = newDeck
+
+#print(deck)
+
+print(deck.index(2019))
